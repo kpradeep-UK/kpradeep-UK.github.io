e12571e520ca2d4bb43be718c18da79a402facdc
Pradeep Kumar K <87231694+kpradeep-UK@users.noreply.github.com>
2025-12-25 21:51:15 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+const observer = new IntersectionObserver(
+  entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.style.opacity = 1;
+        entry.target.style.transform = "translateY(0)";
+      }
+    });
+  },
+  { threshold: 0.15 }
+);
+
+document.querySelectorAll(".fade").forEach(el => observer.observe(el));
